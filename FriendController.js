51bea61ad33a5568731d37c64383c4e87f7ab197
Anton Bredl <antonbr13@gmail.com>
2016-04-05 13:15:27 -0600

    finished this project - i need to look at using ng-options a bit more

--- FriendController.js
+++ FriendController.js
@@ -1,5 +1,28 @@
+angular.module('myApp')
+
+.controller('mainCtrl', function($scope, mainService) {
+
+
+   $scope.friends = mainService.getData();
+
+   $scope.sortPredicate = "name";
+   $scope.sortDescend = false;
+   $scope.predicate = function () {
+    return $scope.sortDescend + $scope.sortPredicate;
+};
+
+   $scope.sortOptions = [{
+                          "Name": "name",
+                          "#Friends": "friend_count",
+                          "City": "current_location.city",
+                          "State": "current_location.state",
+                          "Country": "current_location.country"
+                         },
+                         {
+                          "Ascending": false,
+                          "Descending": true
+                         }];
 
 
 
-
-
+});
